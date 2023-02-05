8c5f83b21e4dd5021c4b630580d7820ea5b6ca13
Ohav Eden <ohav88@gmail.com>
2023-02-05 21:07:58 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let carouselDiv = document.querySelector('.carousel-div')
-let cardsDiv = document.querySelector('.cards-div')
+let cardsDiv = document.querySelector('.cards')
 let aboutSection = document.querySelector('.about')
+let aboutUs = document.querySelector('.aboutus')
 let galleryBtn = document.querySelector('.gallery-btn')
 let aboutBtn = document.querySelector('.about-btn')
 let contactBtn = document.querySelector('.contact-btn')
@@ -19,19 +20,22 @@ logo.addEventListener('click', () => {
 galleryBtn.addEventListener('click', () => {
     carouselDiv.classList.add('hide')
     aboutSection.classList.add('hide')
+    aboutUs.classList.add('hide')
     cardsDiv.classList.remove('hide')
 })
 
 aboutBtn.addEventListener('click', () => {
     carouselDiv.classList.add('hide')
-    aboutSection.classList.remove('hide')
+    aboutSection.classList.add('hide')
+    aboutUs.classList.remove('hide')
     cardsDiv.classList.add('hide')
 })
 
 contactBtn.addEventListener('click', () => {
     carouselDiv.classList.add('hide')
-    about.classList.add('hide')
-    cardsDiv.classList.remove('hide')
+    aboutUs.classList.add('hide')
+    aboutSection.classList.add('hide')
+    cardsDiv.classList.add('hide')
 
 })
 
